65f66fdffdc378910142de1cb825d7b9f9d5cb9d
Taweechai Yuenyang <krumii.it@gmail.com>
2022-03-18 09:37:50 +0700

    add unique

--- database/migrations/2022_03_17_164530_create_investments_table.php
+++ database/migrations/2022_03_17_164530_create_investments_table.php
@@ -19,18 +19,18 @@ return new class extends Migration
             $table->char('trend_id', 21);
             $table->decimal('last_price', 64, 18);
             $table->decimal('profit_percent', 8, 2);
-            $table->string('order_id');// "id": 2, // order id
+            $table->string('order_id')->unique();// "id": 2, // order id
             $table->string('order_hash');// "hash": "fwQ6dnQWQPs4cbatFSJpMCcKTFR", // order hash
             $table->string('order_side');// "side": "SELL", // order side
             $table->string('order_type');// "type": "limit", // order type
-            $table->decimal('value_rate', 64, 18);// "value":('rate');// "rate": 15000, // rate
-            $table->decimal('fee', 64, 18);// "fee": 35.01, // fee
-            $table->decimal('credit', 62, 18);// "credit": 35.01, // credit used
-            $table->decimal('amount', 62, 18);// "amount": 0.93333334, // amount
-            $table->decimal('receive', 64, 18);// "receive": 14000, // amount to receive
+            $table->decimal('value_rate', 64, 18)->nullable()->default(0);// "value":('rate');// "rate": 15000, // rate
+            $table->decimal('fee', 64, 18)->nullable()->default(0);// "fee": 35.01, // fee
+            $table->decimal('credit', 62, 18)->nullable()->default(0);// "credit": 35.01, // credit used
+            $table->decimal('amount', 62, 18)->nullable()->default(0);// "amount": 0.93333334, // amount
+            $table->decimal('receive', 64, 18)->nullable()->default(0);// "receive": 14000, // amount to receive
             $table->bigInteger('parent_id')->nullable()->default(0);// "parent_id": 1, // parent order id
             $table->bigInteger('super_id')->nullable()->default(0);// "super_id": 1, // super parent order id
-            $table->timestamp('ts');// "ts": 1533834844 // timestamp
+            $table->integer('ts')->nullable()->default(0);// "ts": 1533834844 // timestamp for server
             $table->enum('is_status', ['-', 'opened', 'closed'])->nullable()->default('-');
             $table->boolean('is_sync')->nullable()->default(false);
             $table->timestamps();
